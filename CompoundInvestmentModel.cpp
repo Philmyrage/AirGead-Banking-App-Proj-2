a29196a162ebe1fba4956604e4a3069eb2f9f3d0
Phillip Wood <Phillipwood2023@outlook.com>
2024-11-28 17:11:59 -0500

    Investment calculation for deposits is working correctly.

--- CompoundInvestmentModel.cpp
+++ CompoundInvestmentModel.cpp
@@ -19,21 +19,48 @@ void CompoundInvestmentModel::calculateWithDeposit()
 	double total = iniInvestAmount;
 	double interest = 0.0;
 	double yrEndEarnedInterest = 0.0;
+	double closingBalance = 0.0;
+
 	//for every month we add the deposit and calculate the interest.
 	for (int i = 1; i <= months; ++i)
 	{
-		total = (total + monthlyDeposit) + (total * annualIntrestRate);
+		// add the deposit
+		total = (total + monthlyDeposit);
+		//calculate the interest
 		interest = (total * annualIntrestRate);
 		yrEndEarnedInterest += interest;
+
+		//add the total and interest
+		closingBalance = total + interest;
 		//for every year we add those values to a vector to print to our user.
 		if (i % 12 == 0)
-		{ 
-			yearEndBalanceAmounts.at(count) = total + interest;
+		{
+			yearEndBalanceAmounts.at(count) = total;
 			endEarnedInterestAmounts.at(count) = yrEndEarnedInterest;
+			yearlyClosingBalancesWithDeposits.at(count) = closingBalance;
 			yrEndEarnedInterest = 0.0;
 			count++;
 		}
+
+		//total is equal to closing balance of the previous year....
+		total = closingBalance;
 	}
+
+	////for every month we add the deposit and calculate the interest.
+	//for (int i = 1; i <= months; ++i)
+	//{
+	//	total = (total + monthlyDeposit) + (total * annualIntrestRate);
+	//	interest = (total * annualIntrestRate);
+	//	yrEndEarnedInterest += interest;
+	//	//for every year we add those values to a vector to print to our user.
+	//	if (i % 12 == 0)
+	//	{ 
+	//		yearEndBalanceAmounts.at(count) = total + interest;
+	//		endEarnedInterestAmounts.at(count) = yrEndEarnedInterest;
+	//		yrEndEarnedInterest = 0.0;
+	//		count++;
+	//	}
+	//}
 }
 
 void CompoundInvestmentModel::calculateWithoutDeposit()
